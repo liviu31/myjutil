78fd514dfba817dee21a1e045eca0693b685c81a
liviu <liviu31@gmail.com>
2014-01-31 01:45:30 +0200

    stack trace util

--- src/com/myjutil/util/StringUtil.java
+++ src/com/myjutil/util/StringUtil.java
@@ -1,5 +1,8 @@
 package com.myjutil.util;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 /**
  * User: liviu
  * Date: 12/12/13
@@ -12,6 +15,13 @@ public class StringUtil {
             return arg.substring(0, 1).toUpperCase() + arg.substring(1, arg.length());
         }
         return null;
+    }
+
+    //TODO move this
+    public static String getStackTrace(Throwable t) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        t.printStackTrace(new PrintStream(stream));
+        return stream.toString();
     }
 
 }
