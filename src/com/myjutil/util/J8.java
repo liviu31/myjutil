cf25ec1afd1cd124d9a72113140054d6afbf9285
mywebalias <mydevalias@gmail.com>
2016-04-06 18:11:30 +0300

    small method

--- src/com/myjutil/util/J8.java
+++ src/com/myjutil/util/J8.java
@@ -1,6 +1,7 @@
 package com.myjutil.util;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -18,5 +19,10 @@ public class J8 {
         Map<T, Long> all = input.stream()
                 .collect(groupingBy(Function.<T>identity(), Collectors.counting()));
         return all;
+    }
+
+    public static List<Double> toDoubleList(Collection<Long> in) {
+        return in.stream().mapToDouble(Long::doubleValue).boxed()
+                .collect(Collectors.toList());
     }
 }
